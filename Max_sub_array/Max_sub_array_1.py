817bbaedb7bce0a9d3314ff7680a1c3c168ad50c
buptxiaomiao <buptwjh@outlook.com>
2016-03-27 03:13:44 +0800

    make negtive avilable

--- Max_sub_array/Max_sub_array_1.py
+++ Max_sub_array/Max_sub_array_1.py
@@ -10,11 +10,11 @@ from copy import deepcopy
 a = [-3,5,-8,2,7,10,-1,-2]
 b = [10,9,5,-2,1000]
 c = [-3,7,9,-6,0,8,-2,3,-5,9,-8,12,7]
-
+d = [-1,-2,-3,-4,-5,-6,-7]
 def Max_sub_array(*array):
 	temp_sum = 0
 	temp_arr = []
-	max_sum = 0
+	max_sum = -10000
 	max_arr = []
 	for each in array:
 		if temp_sum > 0:#之前的几个数是正的=>加上去#temp_sum + each>each
@@ -32,3 +32,4 @@ if __name__ == '__main__':
 	print Max_sub_array(*b)
 	print Max_sub_array(*a)
 	print Max_sub_array(*c)
+	print Max_sub_array(*d)
